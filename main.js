a0e68d1fabee00019d59e6cdd41e8613d3ba8a55
unknown <abumohammadnayemkhan@gmail.com>
2023-07-11 22:33:38 +0600

    learning javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+//==========Object start==========//
+// var person = {
+//     name: "Nayeem",
+//     age: 26,
+//     isEmpty: true,
+// }
+
+// console.log(person.age);
+//==========object ended==========//
+
+//==========array concate start==========//
+// var arrayOne = ["a", "b"];
+// var arrayTwo = [1, 2];
+// var concateArray = arrayOne.concat(arrayTwo);
+// console.log(concateArray);
+//==========array concate ended==========//
+
+//========== ==========//
