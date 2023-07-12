46682e3a845fa0a59379f4a29d53e700719cffc3
unknown <abumohammadnayemkhan@gmail.com>
2023-07-12 21:52:52 +0600

    learing JavaScript

--- main.js
+++ main.js
@@ -9,10 +9,22 @@
 //==========object ended==========//
 
 //==========array concate start==========//
-// var arrayOne = ["a", "b"];
-// var arrayTwo = [1, 2];
-// var concateArray = arrayOne.concat(arrayTwo);
-// console.log(concateArray);
+//var arrayOne = ["a", "b"];
+//var arrayTwo = [1, 2];
+//var concateArray = arrayOne.concat(arrayTwo);
+//console.log(concateArray);
 //==========array concate ended==========//
 
-//========== ==========//
+//==========String to Array Start==========//
+//let fullName = "Abu Md Nayeem Khan";
+//let nameByArr = Array.from(fullName);
+//console.log(nameByArr);
+//==========String to Array Ended==========//
+
+//==========Array Filter Start==========//
+let myArray = [12, 33,65, 63, 87, 77, 76, 56, 30];
+let result = myArray.find(function(items){
+    return 33 < items;
+});
+console.log(result);
+//==========Array Filter Ended==========//
